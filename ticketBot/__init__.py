1c187e568011f83220358f5b1bd3a2710f594b2b
顏廷宇 <tyyen@sakais-MacBook-Pro.local>
2021-03-25 16:04:24 +0800

    add config yml

--- ticketBot/__init__.py
+++ ticketBot/__init__.py
@@ -1,24 +1,17 @@
 import os
 import sys
-from splinter.browser import Browser
 from time import sleep
 from datetime import datetime
 import schedule
+import yaml
 
-username = 'Frank'
-lastName = 'Yen'
+with open('config.yml') as c:
+    config = yaml.load(c)['Config']
 
-birth = '08/05/1996'
+buy_Tickets = Buy_Tickets(config['username'], config['password'], config['homePage'], config['ticketPage'], config['executable_path']
+                        , config['ticketCount'])
 
-number = "frank0804"
 
-email = 'y95847@gmail.com'
-zipCode = '78731'
-phone = "7737663971"
-
-date = '5/1/2021'
-
-buy_Tickets = Buy_Tickets(username, lastName, birth, number, email, zipCode, phone, date, sys.argv[1])
 schedule.every().day.at("19:58").do(job, buy_Tickets)
 schedule.every().day.at("20:00").do(buying, buy_Tickets)
 
