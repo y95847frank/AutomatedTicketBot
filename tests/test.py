859117a5db7fa8ef4235e83a704b8de1fd50cd70
顏廷宇 <tyyen@sakais-MacBook-Pro.local>
2021-03-26 15:20:55 +0800

    add testing program

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+import AutoTicketsBot as tBot
+
+configDestination = 'config.yml'
+
+args = tBot.addArgs()
+config = tBot.configRead(configDestination)
+if tBot.configWrite(configDestination, args, config) is True:
+	print("Successfully store new config to {}".format(configDestination))
+
+ticketsBot = tBot.AutoTicketsBot(config)
+
+#scheduleBot(ticketsBot, config['Config']['startTime'])
+
+try:
+	tBot.websiteSignIn(ticketsBot, retryCounter=3)
+	tBot.buyTickets(ticketsBot)
+	tBot.notifyUser('AutoTicketsBot Notification', 'Got tickets!!!!!')
+	tBot.terminateBot(ticketsBot, waitTime=600)
+
+except RuntimeError as e:
+	tBot.terminateBot(ticketsBot, waitTime=0)
+	print(e)
