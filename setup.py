5c804ae5364e86202cdc8a120bcdd3290f265910
顏廷宇 <tyyen@sakais-MacBook-Pro.local>
2021-03-26 15:06:19 +0800

    add package setup tool

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='AutoTicketsBot',
+      version='0.0.1',
+      description='A python package to automatically reserve tickets on a Chinese ticket selling website.',
+      url='https://github.com/y95847frank/AutomatedTicketBot',
+      author='y95847frank',
+      author_email='y95847@gmail.com',
+      license='MIT',
+      packages=[
+          'AutoTicketsBot'
+      ],
+      install_requires=[
+          'splinter',
+          'schedule',
+          'yaml'
+      ],
+      python_requires='>=3',
+      )
